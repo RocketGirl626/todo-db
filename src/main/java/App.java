16d8a7c5b7715e22e5b9d0204e1a6ce31b261115
Jeanette Fairless <jfairless@spectrumhomeservices.com>
2015-07-30 12:05:37 -0700

    created tasks are not in database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import spark.ModelAndView;
@@ -14,6 +15,26 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-  }
 
+    post("/tasks", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      ArrayList<Task> tasks = request.session().attribute("Tasks");
+
+      if (tasks == null) {
+        tasks = new ArrayList<Task>();
+        request.session().attribute("tasks", tasks);
+      }
+
+      String description = request.queryParams("description");
+      Integer categoryId = 1;
+      Task newTask = new Task(description, categoryId);
+
+      tasks.add(newTask);
+
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+     }, new VelocityTemplateEngine());
+
+  }
 }
