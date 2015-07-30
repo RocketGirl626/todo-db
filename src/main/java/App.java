96bf74ed62242fa07ff6f3111cb3bd1f243699ac
Jeanette Fairless <jfairless@spectrumhomeservices.com>
2015-07-30 15:05:59 -0700

    everything functioning, all tests passing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,25 +13,18 @@ public class App {
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
+      model.put("tasks", Task.all());
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     post("/tasks", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
 
-      ArrayList<Task> tasks = request.session().attribute("Tasks");
-
-      if (tasks == null) {
-        tasks = new ArrayList<Task>();
-        request.session().attribute("tasks", tasks);
-      }
-
       String description = request.queryParams("description");
       Integer categoryId = 1;
       Task newTask = new Task(description, categoryId);
 
-      tasks.add(newTask);
-
+      newTask.save();
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
      }, new VelocityTemplateEngine());
